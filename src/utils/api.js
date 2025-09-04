a089e6062888dcb8dcc8e49a5c8a9db3ca77d282
Multazam Mahmud <multazam@iut-dhaka.edu>
2025-09-04 13:58:47 +0600

    deletion

--- src/utils/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "/api", // Spring Boot backend
-  withCredentials: true, // important for session cookies
-});
-
-export default api;
